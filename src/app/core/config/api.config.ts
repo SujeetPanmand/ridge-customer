ffd7c23e79f0d6fe2225ad7afce65f7f8750e00e
sujeet.panmand <suraj.sadare@baxture.com>
2023-09-25 18:33:51 +0530

    api call basics

--- src/app/core/config/api.config.ts
+++ src/app/core/config/api.config.ts
@@ -1,6 +1,12 @@
 import { environment } from 'src/environments/environment';
-
+const authEndPoint = [
+  {
+    name: 'USER_LOGIN',
+    method: 'POST',
+    url: environment.baseUrl + '/api/auth/login',
+  },
+];
 export const ApiConfig = {
   baseUrl: '',
-  endpoints: [],
+  endpoints: [...authEndPoint],
 };
