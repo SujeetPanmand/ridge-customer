e780df69ff5b8e45c8b5678cc77f154bc579fa12
sujeet.panmand <suraj.sadare@baxture.com>
2023-09-28 16:57:29 +0530

    make payment page created

--- src/app/features/shop/cart/cart.component.ts
+++ src/app/features/shop/cart/cart.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.scss']
+  styleUrls: ['./cart.component.scss'],
 })
 export class CartComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  makePayment() {
+    this.router.navigate(['shop/cart/checkout']);
   }
-
 }
