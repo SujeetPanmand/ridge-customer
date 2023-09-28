8ce049cc5eda17c38018be34c75e308dd1ac0c55
prachi.kshirsagar <prachi.kshirsagar@baxture.com>
2023-09-28 12:42:11 +0530

    Product details page design

---
+++ src/app/features/product-details/product-details.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { ProductDetailsComponent } from './product-details.component';
+import { ProductDetailsRoutingModule } from './product-details-routing.module';
+@NgModule({
+  declarations: [ProductDetailsComponent],
+  imports: [CommonModule, ProductDetailsRoutingModule, SharedModule],
+})
+export class ProductDetailsModule {}
