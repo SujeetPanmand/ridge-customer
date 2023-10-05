7928d4accbe3e8ee89ef574cb5549f2b1fb17b76
Aniket <aniket.kamble@baxture.com>
2023-10-05 16:27:17 -0700

    Blog details design completed

--- src/app/features/blog/blog-details/blog-details.component.ts
+++ src/app/features/blog/blog-details/blog-details.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-blog-details',
   templateUrl: './blog-details.component.html',
-  styleUrls: ['./blog-details.component.css']
+  styleUrls: ['./blog-details.component.scss']
 })
 export class BlogDetailsComponent implements OnInit {
 
