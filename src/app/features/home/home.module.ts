1c22341ebcfc22d04beab66f1cfb5f4ed2a2eb19
sujeet.panmand <suraj.sadare@baxture.com>
2023-10-05 12:28:24 +0530

    unnecessary home folder deleted

--- src/app/features/home/home.module.ts
+++ src/app/features/home/home.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from 'src/app/shared/shared.module';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './home.component';
 import { HomeRoutingModule } from './home-routing.module';
 import { SlickCarouselModule } from 'ngx-slick-carousel';
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, HomeRoutingModule, SharedModule,SlickCarouselModule],
+  imports: [CommonModule, HomeRoutingModule, SharedModule, SlickCarouselModule],
 })
 export class HomeModule {}
