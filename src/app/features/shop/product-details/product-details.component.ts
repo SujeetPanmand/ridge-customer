1d2244c27731a0c1c158f5c40dd5527c75db3809
sujeet.panmand <suraj.sadare@baxture.com>
2023-10-05 17:01:57 +0530

    minor changes

--- src/app/features/shop/product-details/product-details.component.ts
+++ src/app/features/shop/product-details/product-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ApiService } from 'src/app/shared/services/api.service';
+import { productList } from '../shop.config';
 
 @Component({
   selector: 'app-product-details',
@@ -8,13 +9,20 @@ import { ApiService } from 'src/app/shared/services/api.service';
   styleUrls: ['./product-details.component.scss'],
 })
 export class ProductDetailsComponent implements OnInit {
+  productList = productList;
+  selctedProduct;
   constructor(private route: ActivatedRoute, private apiService: ApiService) {}
   ngOnInit() {
     //this.getProductDetails();
+    this.selctedProduct = this.productList.find(
+      (x) => x == this.route.snapshot.params['productId']
+    );
   }
   getProductDetails() {
     this.apiService
-      .request('GET_PRODUCT_DETAILS', { params: { id: '' } })
+      .request('GET_PRODUCT_DETAILS', {
+        params: { id: this.route.snapshot.params['productId'] },
+      })
       .subscribe((res) => {
         if (res) {
           console.log(res);
