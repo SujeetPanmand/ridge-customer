ebec753c83df240f1b32b99220ff2b332c756e98
prachi.kshirsagar <prachi.kshirsagar@baxture.com>
2023-10-05 12:18:23 +0530

    Blog details component created

--- src/app/features/blog/blog-routing.module.ts
+++ src/app/features/blog/blog-routing.module.ts
@@ -1,11 +1,16 @@
 import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { BlogComponent } from './blog/blog.component';
+import { BlogDetailsComponent } from './blog-details/blog-details.component';
 
 const routes: Routes = [
   {
     path: '',
     component: BlogComponent,
+  },
+  {
+    path: 'blog/blog-deetails',
+    component: BlogDetailsComponent,
   },
 ];
 
--- src/app/features/blog/blog.module.ts
+++ src/app/features/blog/blog.module.ts
@@ -3,8 +3,9 @@ import { CommonModule } from '@angular/common';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { BlogComponent } from './blog/blog.component';
 import { BlogRoutingModule } from './blog-routing.module';
+import { BlogDetailsComponent } from './blog-details/blog-details.component';
 @NgModule({
-  declarations: [BlogComponent],
+  declarations: [BlogComponent, BlogDetailsComponent],
   imports: [CommonModule, BlogRoutingModule, SharedModule],
 })
 export class BlogModule {}
