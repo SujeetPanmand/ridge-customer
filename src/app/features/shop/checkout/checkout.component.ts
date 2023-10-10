71dea00b0e90e93d978f89e27c52d015892d811a
sujeet.panmand <suraj.sadare@baxture.com>
2023-10-10 17:45:41 +0530

    checkout completed

--- src/app/features/shop/checkout/checkout.component.ts
+++ src/app/features/shop/checkout/checkout.component.ts
@@ -6,8 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./checkout.component.scss'],
 })
 export class CheckoutComponent {
+  TAX_AMOUNT = 70;
+  SHIPPING_AMOUNT = 40;
   isSelfPickUp = false;
-  ngOnInit() {}
+  finalOrderProducts = [];
+  orderTotal = 0;
+  orderSubTotal = 0;
+  ngOnInit() {
+    this.defaultSetting();
+  }
+
+  defaultSetting() {
+    this.finalOrderProducts = JSON.parse(
+      localStorage.getItem('finalOrderProducts')
+    )
+      ? JSON.parse(localStorage.getItem('finalOrderProducts'))
+      : [];
+    this.finalOrderProducts.forEach((x) => {
+      this.orderSubTotal = this.orderSubTotal + x.totalPrice;
+    });
+    this.orderTotal =
+      this.orderSubTotal + this.TAX_AMOUNT + this.SHIPPING_AMOUNT;
+  }
   onChangeType(flag) {
     this.isSelfPickUp = flag;
   }
