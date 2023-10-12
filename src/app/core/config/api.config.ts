f3ce75d51f25f11baf87b63f2cbef43117c20809
sujeet.panmand <suraj.sadare@baxture.com>
2023-10-12 15:12:29 +0530

    order done

--- src/app/core/config/api.config.ts
+++ src/app/core/config/api.config.ts
@@ -11,6 +11,7 @@ const authEndPoint = [
     url: environment.baseUrl + '/api/auth/sign-up',
   },
 ];
+
 export const ApiConfig = {
   baseUrl: '',
   endpoints: [...authEndPoint],
--- src/app/features/shop/checkout/checkout.component.ts
+++ src/app/features/shop/checkout/checkout.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CommonService } from 'src/app/shared/services/common.service';
 
 @Component({
   selector: 'app-checkout',
@@ -12,22 +13,32 @@ export class CheckoutComponent {
   finalOrderProducts = [];
   orderTotal = 0;
   orderSubTotal = 0;
+  constructor(private commonService: CommonService) {}
   ngOnInit() {
     this.defaultSetting();
   }
 
   defaultSetting() {
-    this.finalOrderProducts = JSON.parse(
-      localStorage.getItem('finalOrderProducts')
-    )
-      ? JSON.parse(localStorage.getItem('finalOrderProducts'))
+    let list = JSON.parse(localStorage.getItem('cart'))
+      ? JSON.parse(localStorage.getItem('cart'))
       : [];
+    this.finalOrderProducts = list.filter((x) => x.count !== 0);
     this.finalOrderProducts.forEach((x) => {
-      this.orderSubTotal = this.orderSubTotal + x.totalPrice;
+      this.orderSubTotal = this.orderSubTotal + x.price * x.count;
     });
     this.orderTotal =
       this.orderSubTotal + this.TAX_AMOUNT + this.SHIPPING_AMOUNT;
+    this.setGlobalCartCount(list);
   }
+
+  setGlobalCartCount(addedProducts) {
+    let count = 0;
+    addedProducts.forEach((x) => {
+      count = count + x.count;
+    });
+    this.commonService.addProducts(count);
+  }
+
   onChangeType(flag) {
     this.isSelfPickUp = flag;
   }
