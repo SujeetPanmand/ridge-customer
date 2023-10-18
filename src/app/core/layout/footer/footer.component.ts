f841086bb60bfd46d87c7b925f74e31531563ac7
sujeet.panmand <suraj.sadare@baxture.com>
2023-10-18 16:09:40 +0530

    minor changes

--- src/app/core/layout/footer/footer.component.ts
+++ src/app/core/layout/footer/footer.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
 })
-export class FooterComponent {
-
+export class FooterComponent implements OnInit {
+  date;
+  year;
+  ngOnInit(): void {
+    this.date = new Date();
+    this.year = this.date.getFullYear();
+    console.log(this.year);
+  }
 }
