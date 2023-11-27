3af60858cf315192f2f7649a7fe67f603d5af96c
sujeet.panmand <suraj.sadare@baxture.com>
2023-11-27 12:27:52 +0530

    normal changes

--- src/app/features/account/resetpassword/resetpassword.component.ts
+++ src/app/features/account/resetpassword/resetpassword.component.ts
@@ -6,27 +6,34 @@ import { ToastrService } from 'ngx-toastr';
 @Component({
   selector: 'app-resetpassword',
   templateUrl: './resetpassword.component.html',
-  styleUrls: ['./resetpassword.component.scss']
+  styleUrls: ['./resetpassword.component.scss'],
 })
 export class ResetpasswordComponent implements OnInit {
   resetPasswordForm!: FormGroup;
   isSubmitted: boolean = false;
-  resetPasswordUrl :any;
-  resetPasswordToken: string="";
+  resetPasswordUrl: any;
+  resetPasswordToken: string = '';
 
-  constructor(  private router: Router,
-    private active_route: ActivatedRoute,private apiService: ApiService,
-    private toastrService: ToastrService,) { }
+  constructor(
+    private router: Router,
+    private active_route: ActivatedRoute,
+    private apiService: ApiService,
+    private toastrService: ToastrService
+  ) {}
 
   ngOnInit() {
     this.resetPassword();
     this.resetPasswordUrl = this.router.url;
     this.active_route.paramMap.subscribe((params) => {
-      if (params.get('rt') && this.resetPasswordUrl.includes('reset-password')) {
+      if (
+        params.get('rt') &&
+        this.resetPasswordUrl.includes('reset-password')
+      ) {
         this.resetPasswordToken = params.get('rt');
       }
-  }    )};
-  
+    });
+  }
+
   get passwordMatchError() {
     return (
       this.resetPasswordForm.getError('mismatch') &&
@@ -35,39 +42,37 @@ export class ResetpasswordComponent implements OnInit {
   }
 
   resetPassword() {
-    this.resetPasswordForm = new FormGroup(
-      {
-        newPassword: new FormControl('', [
-          Validators.required,
-          Validators.pattern(
-            '^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$'
-          ),
-        ]),
-        confirmPassword: new FormControl('', [Validators.required,]),
-      },
-    );
+    this.resetPasswordForm = new FormGroup({
+      newPassword: new FormControl('', [
+        Validators.required,
+        Validators.pattern(
+          '^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$'
+        ),
+      ]),
+      confirmPassword: new FormControl('', [Validators.required]),
+    });
   }
 
   onSubmit() {
-      if(this.resetPasswordForm.valid && this.resetPasswordToken !="" ){
-          const apiRequest = {
-            data: {
-              token: this.resetPasswordToken,
-              newPassword: this.resetPasswordForm.get('newPassword')?.value,
-              confirmPassword: this.resetPasswordForm.get('confirmPassword')?.value
-            },
-          };
-      
-          this.apiService.request('RESET_PASSWORD', apiRequest).subscribe((response) => {
-            if (response && response.statusCode === 200) {
-              this.toastrService.success(response?.message);
-              this.router.navigate(['']);
-            } else {
-              this.toastrService.error(response?.message);
-            }
-          });
-        
-      }
-  }
+    if (this.resetPasswordForm.valid && this.resetPasswordToken != '') {
+      const apiRequest = {
+        data: {
+          token: this.resetPasswordToken,
+          newPassword: this.resetPasswordForm.get('newPassword')?.value,
+          confirmPassword: this.resetPasswordForm.get('confirmPassword')?.value,
+        },
+      };
 
+      this.apiService
+        .request('RESET_PASSWORD', apiRequest)
+        .subscribe((response) => {
+          if (response && response.statusCode === 200) {
+            this.toastrService.success(response?.message);
+            this.router.navigate(['account/login']);
+          } else {
+            this.toastrService.error(response?.message);
+          }
+        });
+    }
+  }
 }
