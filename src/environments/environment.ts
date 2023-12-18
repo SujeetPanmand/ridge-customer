d4c59555e0def2a179db7140441c189a1c8b5768
suraj-sadare-baxture <suraj.sadare@baxture.com>
2023-12-18 18:05:06 +0530

    minor changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,6 @@ export const environment = {
   //baseUrl: 'http://72.167.143.221',
   STRIPE_PUBLISH_KEY:
     'pk_test_51O9WabGcoSwmGf10iahmfMmzf33qJBJte0XeOYPDKSvXozZrs68ulgMpuUzdPUPtsqG9HuumAFxalBiy2NVf0Kfj00JSALs2fH',
-  MAIN_SECRETE_KEY:
-    'sk_test_51O9WabGcoSwmGf10FD9X8fGFiimyr2T2YJRfqd8dF6jThZWvBpFbPJa70eROfiKIK78dob2wH2jE2D9Or34mYVWW00IxKmxTit',
+  // STRIPE_PUBLISH_KEY:
+  //   'pk_live_51O9WabGcoSwmGf10exskEUNgoWsejUr4lIELTdRferF864I85LuqWI6n3oOIwPCRyQZhnyASkR1xDR1VoewCPpAS004jYZxabk',
 };
