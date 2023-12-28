311d554032b8225bd6d14e832ae53e86fa763336
suraj-sadare-baxture <suraj.sadare@baxture.com>
2023-12-28 14:27:56 +0530

    minor changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: false,
   baseUrl: 'http://103.134.163.197:9049',
-  //baseUrl: 'http://72.167.143.221',
+  //baseUrl: 'https://ridgeatloafersglory.com',
   STRIPE_PUBLISH_KEY:
     'pk_test_51O9WabGcoSwmGf10iahmfMmzf33qJBJte0XeOYPDKSvXozZrs68ulgMpuUzdPUPtsqG9HuumAFxalBiy2NVf0Kfj00JSALs2fH',
-  // STRIPE_PUBLISH_KEY:
-  //   'pk_live_51O9WabGcoSwmGf10exskEUNgoWsejUr4lIELTdRferF864I85LuqWI6n3oOIwPCRyQZhnyASkR1xDR1VoewCPpAS004jYZxabk',
+  //STRIPE_PUBLISH_KEY:
+  //'pk_live_51O9WabGcoSwmGf10exskEUNgoWsejUr4lIELTdRferF864I85LuqWI6n3oOIwPCRyQZhnyASkR1xDR1VoewCPpAS004jYZxabk',
 };
